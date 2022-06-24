d4a95472ce75d3b13fa62b00deeb9378b49567c8
HashRei <hashrei@protonmail.com>
2022-06-24 12:28:31 +0200

    Add connection to my MongoDB cluster

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose")
+require("dotenv/config")
 
 const app = express();
-
 
 // ROUTES
 app.get("/", (req, res) => {
   res.send("We are on home");
 });
+
+// CONNECT TO DB
+mongoose.connect(process.env.DB_CONNECTION, () => {
+  console.log("Connected to my Mongo DB!!!!!!!!!")
+})
 
 app.listen(3000);
